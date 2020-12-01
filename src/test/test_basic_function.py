e64b65bff573e1630454bb485115ce768441a2f4
Sigurd Schoeler <sschoeler99@gmail.com>
2020-12-01 23:10:13 +0100

    Create agent evaluation script

--- src/test/test_basic_function.py
+++ src/test/test_basic_function.py
@@ -5,6 +5,4 @@ from src.example import basic_function
 
 class TestBasicFunction(TestCase):
     def test_basic_function(self):
-        print("wow")
         assert basic_function() == 42
-
---
+++ src/test/test_evaluate_agent.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+import gym.wrappers.tests
+from gym_minesweeper.agents.random_agent import RandomAgent
+from src.evaluate_agent import evaluate_agent
+
+
+class TestEvaluateAgent(TestCase):
+    def test_evaluate_agent(self):
+        env = gym.make("Minesweeper-v0")
+        env.reset()
+        agent = RandomAgent(env.action_space)
+        evaluate_agent(agent, env)
