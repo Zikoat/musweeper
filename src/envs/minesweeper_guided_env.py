Fix probability calculation when _other is used
and other small fixes

src/envs/minesweeper_guided_env.py
@@ -17,17 +17,21 @@ class MinesweeperGuidedEnv(MinesweeperEnv):
     which contains the probability that each cell is a mine.
     """
 
-    def __init__(self, width=8, height=8, mine_count=10, flood_fill=True, enable_guide=True):
-        super().__init__(width, height, mine_count, flood_fill)
+    def __init__(self, enable_guide=True, **kwargs):
+        super().__init__(debug=False, **kwargs)
+        self.enable_guide = enable_guide
         self.observation_space = gym.spaces.Box(low=np.float32(-2),
                                                 high=np.float32(8),
                                                 shape=(2, self.width, self.height))
-        self.enable_guide = enable_guide
 
     def step(self, action):
         observation, *output = super(MinesweeperGuidedEnv, self).step(action)
-        observation = np.array([observation, self.get_probability_matrix()])
+        observation = self._get_guided_observation()
         return observation, *output
+
+    def _get_guided_observation(self):
+        return np.array([(super()._get_observation()),
+                         self.get_probability_matrix()])
 
     def get_probability_matrix(self):
         if not self.enable_guide or self._game_over():
@@ -35,9 +39,9 @@ class MinesweeperGuidedEnv(MinesweeperEnv):
 
         ansi_board = self.get_ansi_board()
         result = self.api_solve({"board": ansi_board, "total_mines": self.mines_count})
-
-        if "_other" in result:
-            matrix = np.array((self.width, self.height), result["_other"])
+        print(result)
+        if "_other" in result["solution"]:
+            matrix = np.full((self.width, self.height), result["solution"]["_other"])
         else:
             matrix = np.empty((self.width, self.height))
 

src/test/test_minesweeper_guided_env.py
@@ -89,28 +89,30 @@ xxxxxxxxxx""",
                          })
 
     def test_create_probability_matrix_from_solution(self):
-        for i in itertools.product([False, True], repeat=1):
-            env = gym.make("MinesweeperGuided-v0", width=3, height=2, mine_count=1)
-            env.reset()
+        env = gym.make("MinesweeperGuided-v0", width=3, height=4, mine_count=3)
+        env.reset()
 
-            # Remove all the mines
-            env.mines = np.zeros((env.width, env.height))
-            # Plant a mine in the bottom middle cell
-            env.mines[1, 1] = 1
+        # Remove all the mines
+        env.mines = np.zeros((env.width, env.height))
+        # Plant mines
+        env.mines[2, 2] = 1
+        env.mines[0, 1] = 1
 
-            # Open top middle cell
-            env.step(1)
-            # Open top right cell
-            env.step(2)
+        # Open top middle cell
+        env.step(0)
 
-            self.assertEqual(env.render("ansi"), "x11\nxxx")
+        # Open top right cell
+        ob, reward, episode_over, info = env.step(2)
+        print(env.render("ansi"))
+        print(ob)
+        self.assertEqual(env.render("ansi"), "11.\nx21\nxxx\nxxx")
 
-            result = self.env.api_solve({"board": env.get_ansi_board(), "total_mines": env.mines_count})
+        result = self.env.api_solve({"board": env.get_ansi_board(), "total_mines": env.mines_count})
 
-            probability_matrix = env.get_probability_matrix()
-            expected_probability_matrix = np.array([[0, 0], [0, 0.5], [0, 0.5]])
+        probability_matrix = env.get_probability_matrix()
+        expected_probability_matrix = np.array([[0,1,0,1/3],[0,0,1/2,1/3],[0,0,1/2,1/3]])
 
-            np.testing.assert_array_equal(expected_probability_matrix, probability_matrix)
+        np.testing.assert_array_equal(expected_probability_matrix, probability_matrix)
 
 
 if __name__ == '__main__':